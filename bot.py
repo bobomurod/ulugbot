fcf5982ce461b1a6020cacaaf4b87c4c6a4c8e66
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-23 00:59:34 +0300

    second

--- bot.py
+++ bot.py
@@ -6,4 +6,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def start(bot, update):bot.send_message(chat_id=update.message.chat_id, text="Ya bot, pogovori so mnoy")
 
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
 
