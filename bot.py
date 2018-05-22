06878053dc19f72bde398c41a9bdda514e8a9dc9
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-23 00:56:58 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,9 @@
+from telegram.ext import Updater
+updater = Updater(token='TOKEN')
+dispatcher = updater.dispatcher
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def start(bot, update):bot.send_message(chat_id=update.message.chat_id, text="Ya bot, pogovori so mnoy")
+
+
