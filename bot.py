ee27601747b692bcf88a431e2a0a7f07f668904f
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-23 20:24:57 +0300

    Ulugbek san gitni urganishing kerakgit add .!

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from telegram.ext import Updater
 updater = Updater(token='579664849:AAGzf4g6bWtzuE5L2qZygi3Cq39PrlZVxuc')
 dispatcher = updater.dispatcher
 import logging
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(bot, update):bot.send_message(chat_id=update.message.chat_id, text="Ya bot, pogovori so mnoy")
